d7104cebe59db7846810850b032832413ab792dd
Ruruw <alexander.bey@outlook.fr>
2026-03-23 19:37:37 +0100

    Ajout dj-database-url pour PostgreSQL sur railway

--- shoppinglist/settings.py
+++ shoppinglist/settings.py
@@ -13,6 +13,8 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
+import dj_database_url
+
 
 load_dotenv()
 
@@ -82,10 +84,11 @@ WSGI_APPLICATION = 'shoppinglist.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite://db.sqlite3',
+        con_max_age=600,
+        ssl_require=False
+    )
 }
 
 LOGIN_REDIRECT_URL = '/'          # ou 'home'
